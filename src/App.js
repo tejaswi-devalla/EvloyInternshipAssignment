6e47cb31229016971966654271736de80c1353e8
tejaswi <tejaswidevalla12@gmail.com>
2023-08-19 16:25:07 +0530

    evloy

--- src/App.js
+++ src/App.js
@@ -3,26 +3,25 @@ import "./App.css";
 
 function App() {
   const [keyword, setKeyword] = useState("");
-  const [startDateInput, setStartDate] = useState("");
-  const [endDateInput, setEndDate] = useState("");
   const [getData, setData] = useState("");
   const [showData, setShowData] = useState(false);
 
-  const api_key = "AIzaSyAt0PVJ6OONsoIFa147N4sfi5uIlFvbn5k";
+  const api_key = "AIzaSyBjPZ0DW2_8Y6AILM6hBK8jjbAq6k0mQBw";
 
   const onSubmitKeywordForm = async (event) => {
     event.preventDefault();
+    const yearAgoData = new Date();
+    yearAgoData.setFullYear(yearAgoData.getFullYear() - 1);
+    const startDate = yearAgoData;
+    const endDate = new Date();
+    console.log(startDate, endDate);
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${keyword}&publishedAfter=${startDateInput}T00:00:00Z&publishedBefore=${endDateInput}T23:59:59Z&key=${api_key}`
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${keyword}&publishedAfter=${startDate.toISOString()}&publishedBefore=${endDate.toISOString()}&key=${api_key}`
     );
     const data = await response.json();
+    console.log(data);
     const responseVolume = data.pageInfo.totalResults;
-    const days =
-      Math.floor(
-        (new Date(endDateInput) - new Date(startDateInput)) /
-          (1000 * 60 * 60 * 24)
-      ) + 1;
-    const averageDailySearchOfKeyword = responseVolume / days;
+    const averageDailySearchOfKeyword = responseVolume / 12;
     setData(averageDailySearchOfKeyword);
     setShowData(true);
   };
@@ -38,35 +37,13 @@ function App() {
         className="keyword-input"
         onChange={(event) => setKeyword(event.target.value)}
       />
-      <label htmlFor="startDate" className="date-text">
-        Enter Start Date
-      </label>
-      <input
-        value={startDateInput}
-        type="date"
-        placeholder="Enter Start Date"
-        id="startDate"
-        className="keyword-input"
-        onChange={(event) => setStartDate(event.target.value)}
-      />
-      <label htmlFor="endDate" className="date-text">
-        Enter End Date
-      </label>
-      <input
-        value={endDateInput}
-        type="date"
-        placeholder="Enter End Date"
-        id="endDate"
-        className="keyword-input"
-        onChange={(event) => setEndDate(event.target.value)}
-      />
       <button type="submit" className="submit-btn">
         Submit
       </button>
       {showData ? (
         <p className="final-text">
-          Average daily search keyword volume for "{keyword}" since{" "}
-          {endDateInput} = {getData.toFixed(2)} search/day
+          Average daily search keyword volume for "{keyword}" per month ={" "}
+          {getData.toFixed(2)}
         </p>
       ) : (
         ""
